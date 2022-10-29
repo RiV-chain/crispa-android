fb77eb0f6f330a2becc6db5e341220bdf24e087e
vadym <vadym.vikulin@gmail.com>
2022-10-29 23:55:58 +0300

    1. peer info object fix

--- app/src/main/java/org/rivchain/app/crispa/models/config/Peer.kt
+++ app/src/main/java/org/rivchain/app/crispa/models/config/Peer.kt
@@ -4,14 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class Peer (
     //Example [{"Key":"JQZIX3KIamcp/6S9rycKiAGyg9MK7U6h8UUY5ej36fY=","Root":"AAABERGfllXfKNJshDs/8uzKEIFkFEccE16dmZV/cAo=","Coords":[2,4],"Port":1,"Remote":"tcp://[fe80::5207:4518:4378:7f1%wlan0]:57541","IP":"202:d7cd:bd04:6bbc:acc6:b002:da12:86c7"},{"Key":"DCNBiKAV1xr72JAFUgNrOYfY6Qm/f0Nq6ESZTSLn1eo=","Root":"AAABERGfllXfKNJshDs/8uzKEIFkFEccE16dmZV/cAo=","Coords":[2,4,1],"Port":2,"Remote":"tcp://[fe80::1c39:839:90a5:6ef%wlan0]:1108","IP":"204:7b97:ceeb:fd45:1ca0:84ed:ff55:bf92"}]
-    @SerializedName("address") var address : String,
-    @SerializedName("key") var key : String,
-    @SerializedName("priority") var priority : Int,
-    @SerializedName("coords") var coords : Array<Int>,
-    @SerializedName("port") var port : Int,
-    @SerializedName("remote") var remote : String,
-    @SerializedName("bytes_recvd") var bytes_recvd : Long,
-    @SerializedName("bytes_sent") var bytes_sent : Long,
-    @SerializedName("uptime") var uptime : Float
+    @SerializedName("IP") var address : String,
+    @SerializedName("Root") var root : String,
+    @SerializedName("Key") var key : String,
+    @SerializedName("Priority") var priority : Int,
+    @SerializedName("Coords") var coords : Array<Int>,
+    @SerializedName("Port") var port : Int,
+    @SerializedName("Remote") var remote : String,
+    @SerializedName("RXBytes") var bytes_recvd : Long,
+    @SerializedName("TXBytes") var bytes_sent : Long,
+    @SerializedName("Uptime") var uptime : Float
 
 )
